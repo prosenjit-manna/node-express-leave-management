bec8671b881ca49d4f0b2b3a039d96500f95603c
Prosenjit Manna <prosenjit@itobuz.com>
2024-04-07 09:52:09 +0530

    user role attached when register completed

--- src/controller/auth/register.ts
+++ src/controller/auth/register.ts
@@ -4,12 +4,14 @@ import { LoginResponse } from '../../apiModel/login/loginResponse.interface';
 import { userModel } from '../../models/userModel';
 import bcrypt from 'bcrypt';
 import { Role } from '../../apiModel/roles.enum';
+import { roleModel } from '../../models/rolesModel';
 
 export async function registerController({ body }: { body: LoginRequest }, res: Response) {
   try {
     const { username, password } = body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new userModel({ username, password: hashedPassword, role: Role.USER });
+    const role = await roleModel.findOne({ name: Role.USER });
+    const user = new userModel({ username, password: hashedPassword, role: Role.USER, roleId: role?._id });
     await user.save();
     res.status(201).json({ message: 'User registered successfully' }) as LoginResponse;
   } catch (error: any) {
