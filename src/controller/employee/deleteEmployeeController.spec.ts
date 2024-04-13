66baa247aeea7717ef41ae1a2dcf68c6c9ac1bb4
Prosenjit Manna <prosenjit@itobuz.com>
2024-04-13 17:49:41 +0530

    e2e: delete employee added

--- src/controller/employee/deleteEmployeeController.spec.ts
+++ src/controller/employee/deleteEmployeeController.spec.ts
@@ -1,42 +1,58 @@
-import { faker } from "@faker-js/faker"
-import { EmployeeRequest } from "../../interface/api/employee/add-employee/add-employee-request.schema"
-import { getAuthenticatedClient } from "../../lib-e2e/getAuthenticatedClient"
-import { get_env } from "../../lib/get-env"
-import { AxiosInstance } from "axios"
-import { userModel } from "../../models/userModel"
-import { dbConnect } from "../../lib/connection"
-import mongoose from "mongoose"
-import { UserType } from "../../interface/data/userType.enum"
-import { employeeModel } from "../../models/employeeModel"
-import { EditEmployeeRequest } from "../../interface/api/employee/edit-employee/editRequest.schema"
-import { viewEmployeeRequest } from "../../interface/api/employee/view-employee/view-employee-request.schema"
-import { DeleteEmployeeRequest } from "../../interface/api/employee/delete-employee/delete-employee-request.schema"
+import { getAuthenticatedClient } from '../../lib-e2e/getAuthenticatedClient';
+import { AxiosInstance } from 'axios';
+import { userModel } from '../../models/userModel';
+import { dbConnect } from '../../lib/connection';
+import mongoose from 'mongoose';
+import { UserType } from '../../interface/data/userType.enum';
+import { DeleteEmployeeRequest } from '../../interface/api/employee/delete-employee/delete-employee-request.schema';
+import { employeeModel } from '../../models/employeeModel';
 
-describe('Add Employee', () => {
-  let ownerClient: AxiosInstance;
-  let mongoInstance: typeof mongoose | undefined;
-  let userId: string;
+[UserType.APP_OWNER, UserType.ORG_OWNER, UserType.USER].forEach(async (userType) => {
+  describe(`Delete Employee ${userType}`, () => {
+    let testClient: AxiosInstance | null;
+    let mongoInstance: typeof mongoose | undefined;
 
-  beforeAll(async () => {
-    mongoInstance = await dbConnect();
+    let testData = {
+      userId: '',
+      employeeId: '',
+    };
+
+    beforeAll(async () => {
+      mongoInstance = await dbConnect();
+    });
+    afterAll(() => {
+      if (mongoInstance) {
+        mongoInstance.disconnect();
+      }
+      testClient = null;
+    });
+
+    test('Login', async () => {
+      testClient = await getAuthenticatedClient(userType);
+    });
+
+    test('Delete Employee', async () => {
+      const query = {
+        $or: [{ deletedAt: { $eq: null } }, { deletedAt: { $exists: true, $eq: undefined } }],
+      };
+      const employee = await employeeModel.findOne(query).limit(1);
+      console.log(employee);
+
+      testData.userId = employee?.userId?.toString() as string;
+
+      const payload: DeleteEmployeeRequest = {
+        userId: testData.userId,
+      };
+
+      try {
+        await testClient?.post('/employee/delete', payload);
+      } catch (e: any) {
+        if (userType === UserType.USER) {
+          expect(e.message).toBe('Access Forbidden!');
+        } else {
+          expect(e).toBeUndefined();
+        }
+      }
+    });
   });
-  afterAll(() => {
-    if (mongoInstance) {
-      mongoInstance.disconnect();
-    }
-  });
-
-  test('Login', async () => {
-    ownerClient = await getAuthenticatedClient(UserType.APP_OWNER);
-  })
-
-  test('Delete Employee', async () => {
-    console.log(userId);
-    const payload: DeleteEmployeeRequest = {
-      userId,
-    }
-    await ownerClient.post('/employee/delete', payload);
-  })
-
-
-})
+});
