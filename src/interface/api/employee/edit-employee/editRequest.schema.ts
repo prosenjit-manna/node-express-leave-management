c3257746548b967d47480ce3971388bf1fec9f52
Prosenjit Manna <prosenjit@itobuz.com>
2024-04-15 13:09:55 +0530

    fix: edit employee

--- src/interface/api/employee/edit-employee/editRequest.schema.ts
+++ src/interface/api/employee/edit-employee/editRequest.schema.ts
@@ -1,13 +1,15 @@
 import { z } from 'zod';
 
-export const editEmployeeRequestSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  phone: z.string(),
-  gender: z.string().refine((v) => ['M', 'F'].includes(v), {
-    message: 'Invalid gender',
-  }),
-  dob: z.string(),
-});
+export const editEmployeeRequestSchema = z
+  .object({
+    id: z.string(),
+    name: z.string(),
+    phone: z.string(),
+    gender: z.string().refine((v) => ['M', 'F'].includes(v), {
+      message: 'Invalid gender',
+    }),
+    dob: z.string(),
+  })
+  .strict();
 
 export type EditEmployeeRequest = z.infer<typeof editEmployeeRequestSchema>;
