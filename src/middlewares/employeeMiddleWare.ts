9bde7816f83b65f4648ff879fd3c242866a6d17b
Prosenjit Manna <prosenjit@itobuz.com>
2024-04-15 14:50:41 +0530

    employee view routes privileges update

--- src/middlewares/employeeMiddleWare.ts
+++ src/middlewares/employeeMiddleWare.ts
@@ -4,6 +4,7 @@ import { sendErrorResponse, sendForbiddenResponse } from '../lib/sendResponse';
 import { EmployeeRequest } from '../interface/api/employee/add-employee/add-employee-request.schema';
 import { EditEmployeeRequest } from '../interface/api/employee/edit-employee/editRequest.schema';
 import { DeleteEmployeeRequest } from '../interface/api/employee/delete-employee/delete-employee-request.schema';
+import { viewEmployeeRequest } from '../interface/api/employee/view-employee/view-employee-request.schema';
 
 export async function employeeMiddleWare(req: Request, res: Response, next: NextFunction) {
   const action = req.path.replace('/', '');
@@ -44,7 +45,11 @@ export async function employeeMiddleWare(req: Request, res: Response, next: Next
     }
     next();
   } else if (action === 'view') {
-    if (!req.privileges.employee?.list?.enabled) {
+    const body: viewEmployeeRequest = req.body;
+    const row = await employeeModel.findOne({ userId: body.employeeId });
+    const is_matched_author = req.user._id?.toString() === row?.userId;
+    const is_document_owner = req.privileges?.employee?.delete?.createdByOnly && is_matched_author;
+    if ((req.privileges.employee?.list?.createdByOnly && !is_document_owner) || !req.privileges.employee?.list?.enabled) {
       return sendForbiddenResponse({ res });
     }
     next();
