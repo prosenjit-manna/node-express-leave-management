fee02f46fd140b66940d6ab970fe8c5a8584334b
Prosenjit Manna <prosenjit@itobuz.com>
2024-04-16 18:28:04 +0530

    add leave test added

--- src/controller/leave/addLeaveController.spec.ts
+++ src/controller/leave/addLeaveController.spec.ts
@@ -5,7 +5,7 @@ import { dbConnect } from '../../lib/connection';
 import mongoose from 'mongoose';
 import { UserType } from '../../interface/data/userType.enum';
 import { employeeModel } from '../../models/employeeModel';
-import { AddLeaveRequest, addLeaveRequestSchema } from '../../interface/api/leave/add/addLeaveRequest.schema';
+import { AddLeaveRequest } from '../../interface/api/leave/add/addLeaveRequest.schema';
 import { LeaveType } from '../../interface/data/leaveType.enum';
 
 [UserType.APP_OWNER, UserType.ORG_OWNER, UserType.USER].forEach(async (userType) => {
@@ -29,21 +29,21 @@ import { LeaveType } from '../../interface/data/leaveType.enum';
 
     test('Add Leave', async () => {
       const currentUser: any = await testClient?.get('/auth/current-user');
-
-      let employee = await employeeModel.findOne({
+      let userId: any;
+      const employee = await employeeModel.findOne({
         name: { $regex: /a|b|c|d/i }, // Regular expression to match names containing 'a', 'b', 'c', or 'd' (case-insensitive)
         deletedAt: { $in: [null, undefined] }, // Matches documents where 'deletedAt' is either null or undefined
         userId: { $nin: [new mongoose.Types.ObjectId(currentUser?.user?._id)] },
       });
 
+      userId = employee?._id.toString();
+
       if (userType === UserType.USER) {
-        employee = await employeeModel.findOne({
-          userId: new mongoose.Types.ObjectId(currentUser?.user?._id),
-        });
+        userId = currentUser?.user._id;
       }
 
       const payload: AddLeaveRequest = {
-        userId: String(employee?.userId),
+        userId: userId,
         leaveStart: new Date().toISOString(),
         leaveEnd: new Date().toISOString(),
         count: 1,
